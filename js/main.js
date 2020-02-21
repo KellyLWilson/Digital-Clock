b90b83b70d84c041a609848dc2afbc92e8179918
KellyLWilson <60358839+KellyLWilson@users.noreply.github.com>
2020-02-21 17:45:54 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ function date_time(id)
         date = new Date;
         year = date.getFullYear();
         month = date.getMonth();
-        months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December'];
         d = date.getDate();
         day = date.getDay();
-        days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+        days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         h = date.getHours();
         if(h<10)
         {
